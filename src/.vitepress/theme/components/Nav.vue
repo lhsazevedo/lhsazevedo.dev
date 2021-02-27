047ebc8f7c91113bfdd9b25a0f5b7877434631ab
Lucas H. Azevedo <lhs_azevedo@hotmail.com>
2021-02-27 03:18:39 -0300

    first commit

---
+++ src/.vitepress/theme/components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+  <nav class="nav-home">
+    <ul class="list">
+      <li class="item">
+        <a href="/blog" class="link">Blog</a>
+      </li>
+
+      <li class="item">
+        <a href="/projects" class="link">Projects</a>
+      </li>
+    </ul>
+  </nav>
+</template>
